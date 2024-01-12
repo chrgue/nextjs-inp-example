384bed111ddc2544aad5b73fa6dda9fd08eebb66
Christian Günther <17281422+chrgue@users.noreply.github.com>
2024-01-12 17:50:53 +0100

    useTransition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,25 @@
 import ReactLogo from "@/components/ReactLogo";
 import {PokemonList} from "@/components/PokemonList";
 import {Search} from "@/components/Search";
-import {useState} from "react";
+import {useState, useTransition} from "react";
 
 export default function Home() {
     const initialState = "";
     const [filterText, setFilterText] = useState(initialState);
+    const [inputText, setInputText] = useState(initialState);
 
+    const [isPending, startTransition] = useTransition()
     function onChange(text: string) {
-        setFilterText(text);
+        startTransition(() => {
+            setFilterText(text);
+        });
+        setInputText(text);
     }
 
     return (
         <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
             <ReactLogo src="/logo.svg" alt="React logo"/>
-            <Search text={filterText} onChange={(text) => onChange(text)}/>
+            <Search text={inputText} onChange={(text) => onChange(text)}/>
             <PokemonList text={filterText}/>
         </div>
     );
